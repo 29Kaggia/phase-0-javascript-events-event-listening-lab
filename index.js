162f7d0e417759465e83ec35b8302fe918998c9e
Lewis <lewis.kagia@student.moringaschool.com>
2023-06-22 12:36:43 +0300

    event listeners lab

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
-function addingEventListener() {
-}
+const sinon = require('sinon');
+const { expect } = require('chai');
+
+describe("index.js", () => {
+  let input;
+
+  beforeEach(function() {
+    const document = window.document; 
+    const button = document.createElement('button');
+    button.id = 'button';
+    document.body.appendChild(button);
+    input = document.getElementById('button');
+    sinon.spy(input, 'addEventListener');
+  });
+
+  it("binds an event listener in addingEventListener()", () => {
+    
+    addingEventListener();
+    expect(input.addEventListener.called).to.be.true;
+  });
+});
